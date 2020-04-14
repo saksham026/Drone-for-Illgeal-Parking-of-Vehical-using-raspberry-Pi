0cdb9faea0ff503e387cea365e536334ecf049e1
saksham <saksham@SAKSHAM>
2020-04-14 10:13:29 +0530

    First commit

---
+++ quadcopter_raspberry_pi/qc.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+###############################################################################################
+###############################################################################################
+##                                                                                           ##
+## Hove's Raspberry Pi Python Quadcopter Flight Controller.  Open Source @ GitHub            ##
+## PiStuffing/Quadcopter under GPL for non-commercial application.  Any code derived from    ##
+## this should retain this copyright comment.                                                ##
+##                                                                                           ##
+## Copyright 2014 - 2018 Andy Baker (Hove) - andy@pistuffing.co.uk                           ##
+##                                                                                           ##
+###############################################################################################
+###############################################################################################
+
+from Quadcopter import Quadcopter
+import os
+
+os.nice(-10)
+
+if __name__ == '__main__':
+	#-------------------------------------------------------------------------------------
+	# Off we go!
+	#-------------------------------------------------------------------------------------
+	Quadcopter().go()
